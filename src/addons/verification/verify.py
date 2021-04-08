ac28f76e579fd72f9ec99c2efc1b5a1e8d687691
xomien <xomien01@gmail.com>
2021-04-08 14:29:04 -0400

    left over

--- src/addons/verification/verify.py
+++ src/addons/verification/verify.py
@@ -91,8 +91,7 @@ class VerifyCommand(Bloxlink.Module):
         else:
             welcome_message, embed = await format_update_embed(roblox_user, author, added=added, removed=removed, errors=errors, warnings=warnings, nickname=nickname if old_nickname != author.display_name else None, prefix=prefix, guild_data=guild_data)
 
-            if embed:
-                await post_event(guild, guild_data, "verification", f"{author.mention} ({author.id}) has **verified** as `{roblox_user.username}`.", GREEN_COLOR)
+            await post_event(guild, guild_data, "verification", f"{author.mention} ({author.id}) has **verified** as `{roblox_user.username}`.", GREEN_COLOR)
 
             await response.send(content=welcome_message, embed=embed)
 
