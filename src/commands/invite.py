628662ee59fa3570f6d24f5b1cfbc8dd42377230
xomien <xomien01@gmail.com>
2021-05-31 02:19:05 -0400

    Add buttons to !invite

--- src/commands/invite.py
+++ src/commands/invite.py
@@ -1,5 +1,5 @@
 from resources.structures.Bloxlink import Bloxlink # pylint: disable=import-error
-from discord import Embed
+import discord
 
 
 @Bloxlink.command
@@ -13,9 +13,8 @@ class InviteCommand(Bloxlink.Module):
     async def __main__(self, CommandArgs):
         response = CommandArgs.response
 
-        embed = Embed(title="Invite Bloxlink")
-        embed.description = "**To add Bloxlink to your server, click this link: https://blox.link/invite\n" \
-                            "Support server: https://blox.link/support**"
+        embed = discord.Embed(title="Invite Bloxlink")
+        embed.description = "To add Bloxlink to your server, click the link below."
 
         embed.add_field(name="Frequently Asked Questions", value="1.) I don't see my server when I try to invite the bot!\n" \
                                                                 f"> There are 2 possibilities:\n> a.) you don't have the `Manage Server` " \
@@ -25,4 +24,8 @@ class InviteCommand(Bloxlink.Module):
 
         embed.set_footer(text="Thanks for choosing Bloxlink!", icon_url=Bloxlink.user.avatar.url)
 
-        await response.send(embed=embed)
+        view = discord.ui.View()
+        view.add_item(item=discord.ui.Button(style=discord.ButtonStyle.link, label="Invite Bloxlink", url="https://blox.link/invite"))
+        view.add_item(item=discord.ui.Button(style=discord.ButtonStyle.link, label="Support Server",  url="https://blox.link/support"))
+
+        await response.send(embed=embed, view=view)
