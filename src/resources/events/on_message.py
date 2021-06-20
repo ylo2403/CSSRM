7c21bad80184c3e0f01ff5dad38ae769fc48a245
xomien <xomien01@gmail.com>
2021-06-20 02:49:40 -0400

    Update on_message.py

--- src/resources/events/on_message.py
+++ src/resources/events/on_message.py
@@ -2,7 +2,6 @@ from ..structures import Bloxlink, Arguments # pylint: disable=import-error
 from ..exceptions import CancelCommand # pylint: disable=import-error
 
 parse_message = Bloxlink.get_module("commands", attrs="parse_message")
-validate_guild = Bloxlink.get_module("utils", attrs=["validate_guild"])
 
 
 @Bloxlink.module
