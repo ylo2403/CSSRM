e74f3a69e705cbce09286505c7d5a0097ad9d560
xomien <xomien01@gmail.com>
2021-06-30 03:51:12 -0400

    Catch CancelCommand exception on member remove event

--- src/resources/events/on_member_remove.py
+++ src/resources/events/on_member_remove.py
@@ -1,7 +1,5 @@
 from ..structures.Bloxlink import Bloxlink # pylint: disable=import-error, no-name-in-module
-from ..constants import DEFAULTS # pylint: disable=import-error, no-name-in-module
-from ..exceptions import UserNotVerified, RobloxDown, RobloxAPIError # pylint: disable=import-error, no-name-in-module
-import discord
+from ..exceptions import CancelCommand # pylint: disable=import-error, no-name-in-module
 
 get_guild_value = Bloxlink.get_module("cache", attrs=["get_guild_value"])
 guild_obligations = Bloxlink.get_module("roblox", attrs=["guild_obligations"])
@@ -16,4 +14,7 @@ class MemberRemoveEvent(Bloxlink.Module):
 
         @Bloxlink.event
         async def on_member_remove(member):
-            await guild_obligations(member, member.guild, join=False, event=True)
+            try:
+                await guild_obligations(member, member.guild, join=False, event=True)
+            except CancelCommand:
+                pass
