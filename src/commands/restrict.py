40cc1b9bddc7c6259eabe93d1505983d6a90f8e4
xomien <xomien01@gmail.com>
2021-11-08 16:06:40 -0500

    Fix /restrict

--- src/commands/restrict.py
+++ src/commands/restrict.py
@@ -248,7 +248,7 @@ class RestrictCommand(Bloxlink.Module):
         if not remove_data_match:
             raise Message("You must select an option from the dropdown!", type="silly")
         else:
-            directory_name, remove_id = remove_data_match.group(0), remove_data_match.group(1)
+            directory_name, remove_id = remove_data_match.group(1), remove_data_match.group(2)
 
         if directory_name and remove_id:
             if restrictions.get(directory_name, {}).get(remove_id):
