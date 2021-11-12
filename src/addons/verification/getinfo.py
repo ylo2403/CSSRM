00854b8cc9d38c433c60b32b1d3bfc528fc2abd2
xomien <xomien01@gmail.com>
2021-11-12 02:55:35 -0500

    Fix mobile /getinfo not selecting option

--- src/addons/verification/getinfo.py
+++ src/addons/verification/getinfo.py
@@ -1,5 +1,6 @@
 from resources.structures.Bloxlink import Bloxlink # pylint: disable=import-error, no-name-in-module, no-name-in-module
-from resources.exceptions import Error, RobloxNotFound, RobloxAPIError, Message, UserNotVerified
+from resources.exceptions import Error, RobloxNotFound, RobloxAPIError, Message, UserNotVerified # pylint: disable=import-error, no-name-in-module, no-name-in-module
+import re
 
 get_user, get_binds = Bloxlink.get_module("roblox", attrs=["get_user", "get_binds"])
 
@@ -30,33 +31,37 @@ class GetInfoCommand(Bloxlink.Module):
         self.slash_only = True
         self.aliases = ["robloxsearch", "rs", "whois"] # FIXME
 
+        self.autocomplete_regex = re.compile("roblox-id-(.*)")
+
     async def auto_complete_roblox_user(self, interaction, command_args, focused_option):
         if not focused_option:
             return []
 
         try:
             if focused_option.isdigit():
-                roblox_user, _ = await get_user(roblox_id=focused_option, cache=True)
+                roblox_user, _ = await get_user(roblox_id=focused_option, cache=False)
             else:
-                roblox_user, _ = await get_user(username=focused_option, cache=True)
+                roblox_user, _ = await get_user("username", "id", username=focused_option, everything=False, basic_details=False, cache=False)
 
         except (RobloxNotFound, RobloxAPIError):
             return []
 
-        return [[f"{roblox_user.username} ({roblox_user.id})", roblox_user.id]]
+        return [[f"{roblox_user.username} ({roblox_user.id})", f"roblox-id-{roblox_user.id}"]]
 
 
     async def __main__(self, CommandArgs):
         response = CommandArgs.response
         guild = CommandArgs.guild
 
-        roblox_id    = CommandArgs.parsed_args["roblox_name"]
+        roblox_info  = CommandArgs.parsed_args["roblox_name"]
         discord_user = CommandArgs.parsed_args["discord_user"]
 
-        if roblox_id and discord_user:
+        roblox_id = roblox_name = None
+
+        if roblox_info and discord_user:
             raise Message("Please only specify a Roblox username OR a Discord user!", type="silly")
 
-        elif not (roblox_id or discord_user):
+        elif not (roblox_info or discord_user):
             discord_user = CommandArgs.author
 
         if guild:
@@ -64,8 +69,15 @@ class GetInfoCommand(Bloxlink.Module):
         else:
             role_binds, group_ids = {}, {}
 
+        roblox_id = self.autocomplete_regex.search(roblox_info)
+
+        if not roblox_id:
+            roblox_name = roblox_info
+        else:
+            roblox_id = roblox_id.group(1)
+
         try:
-            _, _ = await get_user(author=discord_user, roblox_id=roblox_id, group_ids=(group_ids, role_binds), send_embed=True, guild=guild, response=response, everything=True)
+            _, _ = await get_user(author=discord_user, roblox_id=roblox_id, username=roblox_name, group_ids=(group_ids, role_binds), send_embed=True, guild=guild, response=response, everything=True)
         except RobloxNotFound:
             raise Error("This Roblox account doesn't exist.")
         except RobloxAPIError:
