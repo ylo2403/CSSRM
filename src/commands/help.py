31ea9db79fb0c2e1d3e900bba7e6dd986c3f19f3
xomien <xomien01@gmail.com>
2022-05-17 02:08:57 -0400

    /commands -> /help

--- src/commands/help.py
+++ src/commands/help.py
@@ -1,18 +1,21 @@
-from resources.structures.Bloxlink import Bloxlink # pylint: disable=import-error, no-name-in-modules
-from resources.exceptions import CancelCommand
-from discord import File
-from io import BytesIO
+from resources.structures import Bloxlink, InteractionPaginator, Application # pylint: disable=import-error, no-name-in-module, no-name-in-module
+from resources.exceptions import Error # pylint: disable=import-error, no-name-in-module, no-name-in-module
+from resources.constants import OWNER # pylint: disable=import-error, no-name-in-module, no-name-in-module
 
-fetch = Bloxlink.get_module("utils", attrs=["fetch"])
-parse_message = Bloxlink.get_module("commands", attrs="parse_message")
+import discord
+
+get_enabled_addons = Bloxlink.get_module("addonsm", attrs=["get_enabled_addons"])
+commands = Bloxlink.get_module("commands", attrs=["commands"])
+
+
+
 
 @Bloxlink.command
 class HelpCommand(Bloxlink.Module):
-    """view information about Bloxlink"""
+    """view the command list, or get help on a certain command"""
 
     def __init__(self):
-        self.dm_allowed    = True
-        self.slash_enabled = True
+        self.aliases = ["cmds", "commands"]
         self.arguments = [
             {
                 "prompt": "Please specify the command name",
@@ -20,37 +23,76 @@ class HelpCommand(Bloxlink.Module):
                 "name": "command_name"
             }
         ]
-
-        self.images = None
-        self.urls = [
-            "https://i.imgur.com/zQwITet.png",
-            "https://i.imgur.com/yv6jfNC.png",
-            "https://i.imgur.com/pwMB8wZ.png"
-        ]
-
-    async def __setup__(self):
-        self.images = []
-
-        for url in self.urls:
-            image_data, image_response = await fetch(url=url, bytes=True, raise_on_failure=False)
-
-            if image_response.status == 200:
-                self.images.append(image_data)
+        self.dm_allowed = True
+        self.slash_enabled = True
 
     async def __main__(self, CommandArgs):
+        command_name = CommandArgs.parsed_args.get("command_name")
         response = CommandArgs.response
-        channel  = CommandArgs.channel
-        guild    = CommandArgs.guild
-
-        command_name = CommandArgs.parsed_args["command_name"]
+        guild = CommandArgs.guild
+        author = CommandArgs.author
 
         if command_name:
-            await response.command.redirect(CommandArgs, "commands", arguments={"command_name": command_name})
+            command_name = command_name.lower()
 
-            raise CancelCommand
+            for name, command in commands.items():
+                if name == command_name or command_name in command.aliases:
+                    embed = discord.Embed(title=f"/{name}", description=command.full_description or "N/A")
+                    embed.set_author(name="Bloxlink", icon_url=Bloxlink.user.avatar.url)
+                    embed.add_field(name="Category", value=command.category)
 
-        if guild and not channel.permissions_for(guild.me).attach_files:
-            urls = "\n".join(self.urls)
-            await response.send(f"Some general info is below! To view all commands, say `/commands`.\n{urls}")
+                    if command.usage:
+                        embed.add_field(name="Usage", value=f"`/{name} {command.usage}`")
+                    else:
+                        embed.add_field(name="Usage", value=f"`/{name}`")
+
+                    if command.aliases:
+                        if len(command.aliases) > 1:
+                            embed.add_field(name="Aliases", value=", ".join(command.aliases))
+                        else:
+                            embed.add_field(name="Alias", value=", ".join(command.aliases))
+
+                    permissions = command.permissions
+                    permission_text = []
+
+                    if (permissions.premium or command.category == "Premium") and not command.free_to_use:
+                        permission_text.append("**Premium Command**\nThis command can only be used by Bloxlink Premium members.")
+
+                    if permissions.developer_only or command.category == "Developer":
+                        permission_text.append("**Developer Only**\nThis command can only be used by the Bloxlink Developer.")
+
+                    if permissions.function:
+                        permission_text.append("**Custom Permission**\nYou must meet a custom predicate to use this command.")
+
+                    if permission_text:
+                        embed.add_field(name="Permissions", value="\n\n".join(permission_text))
+
+                    if command.examples:
+                        examples = []
+
+                        for example in command.examples:
+                            examples.append(f"/{command_name} {example}")
+
+                        embed.add_field(name="Examples", value="\n".join(examples))
+
+                    await response.send(embed=embed)
+
+                    break
+            else:
+                raise Error(f"This command does not exist! Please use `/help` to view a full list of commands.")
         else:
-            await response.send("Some general info is below! To view all commands, say `/commands`.", files=[File(BytesIO(b), filename=f"image_{i}.png") for i, b in enumerate(self.images)])
+            commands_categories = {}
+            enabled_addons = guild and await get_enabled_addons(guild) or {}
+
+            for command_name, command in commands.items():
+                if (command.hidden or isinstance(command, Application) or (command.addon and str(command.addon) not in enabled_addons)) and author.id != OWNER:
+                    continue
+
+                commands_categories[command.category] = commands_categories.get(command.category) or []
+                commands_categories[command.category].append(f"**[/{command_name}](https://blox.link/commands/)**\n<:Reply:872019019677450240>{command.description}")
+
+            paginator = InteractionPaginator(commands_categories, response, max_items=8, use_fields=False, default_category="Miscellaneous",
+                                            description="Roblox Verification made easy! Features everything you need to integrate your Discord server with Roblox.",
+                                            footer="/help <command name> to view more information | ")
+
+            await paginator()
