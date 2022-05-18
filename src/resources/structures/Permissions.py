6bdf139e219ed411e990f3f9f25e5e9f062ae95a
xomien <xomien01@gmail.com>
2022-05-18 00:50:02 -0400

    Revert "Magic Roles fix"
    This reverts commit 44a4e4c1e488c1740a6cb57a228b9d5e2182167c.

--- src/resources/structures/Permissions.py
+++ src/resources/structures/Permissions.py
@@ -4,13 +4,10 @@ import discord
 class Permissions(discord.Permissions):
     """Contains permission attributes for commands"""
 
-    def __init__(self, function=None, bloxlink_updater=False, bloxlink_admin=False, premium=False, **kwargs):
+    def __init__(self, function=None, **kwargs):
         self.function = function
         self.developer_only = False
-        self.premium = premium
-        self.bloxlink_updater = bloxlink_updater
-        self.bloxlink_admin = bloxlink_admin
-
+        self.premium = False
 
         self.allow_bypass = False
 
